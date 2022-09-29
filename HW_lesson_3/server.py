d8f5cb13afdfcf0b6669e790376027b3ed181adb
AntoshaLev <90862359+AntoshaLev@users.noreply.github.com>
2022-09-29 16:56:41 +0300

    Lesson_4

--- HW_lesson_3/server.py
+++ HW_lesson_3/server.py
@@ -1,42 +1,42 @@
-import chat
-import jim
+from socket import socket, AF_INET, SOCK_STREAM
+from sys import argv
 
-client_name = ''
+from HW_lesson_4.utils import check_port, receive_message, send_message, print_message
+from HW_lesson_4.variables import DEFAULT_API_ADDRESS, DEFAULT_PORT
+
+
+def gen_response(message):
+    return {
+        "response": 200,
+        "alert": ""
+    }
+
+
+def main():
+    addr = DEFAULT_API_ADDRESS
+    port = DEFAULT_PORT
+    if '-a' in argv:
+        try:
+            addr = argv[argv.index('-a') + 1]
+        except IndexError:
+            raise ValueError('после аргумента -a должен быть ip-адресс')
+    if '-p' in argv:
+        try:
+            port = check_port(argv[argv.index('-p') + 1])
+        except IndexError:
+            raise ValueError('после аргумента -p должен быть номер порта')
+
+    socket_server = socket(family=AF_INET, type=SOCK_STREAM)
+    socket_server.bind((addr, port))
+    socket_server.listen()
+
+    socket_client, address_client = socket_server.accept()
+    message = receive_message(socket_client)
+    print_message(message)
+    send_message(socket_client, gen_response(message))
+    socket_server.close()
+    socket_client.close()
+
 
 if __name__ == '__main__':
-    parser = chat.create_parser()
-    namespace = parser.parse_args()
-
-    sock = chat.get_server_socket(namespace.addr, namespace.port)
-
-    serv_addr = sock.getsockname()
-    print(f'Server started at {serv_addr[0]}:{serv_addr[1]}')
-
-    client, address = sock.accept()
-    print(f'Client connected from {address[0]}:{address[1]}')
-
-    while True:
-        data = chat.get_data(client)
-
-        if client_name == '':
-            if data['action'] == 'presence' and data['user']['account_name'] != '':
-                client_name = data['user']['account_name']
-                jim.RESPONSE['response'], jim.RESPONSE['alert'] = jim.SERV_RESP[0]
-                print(f'{data["time"]} - {data["user"]["account_name"]}: {data["user"]["status"]}')
-            else:
-                jim.RESPONSE['response'], jim.RESPONSE['alert'] = jim.SERV_RESP[1]
-
-        if client_name != '' and data['action'] == 'msg':
-            print(f'{data["time"]} - {client_name}: {data["message"]}')
-            jim.RESPONSE['response'], jim.RESPONSE['alert'] = jim.SERV_RESP[0]
-
-            if data["message"] == 'exit':
-                jim.RESPONSE['response'], jim.RESPONSE['alert'] = jim.SERV_RESP[2]
-
-        chat.send_data(client, jim.RESPONSE)
-
-        if jim.RESPONSE['response'] != '200':
-            client.close()
-            break
-
-    sock.close()
+    main()
